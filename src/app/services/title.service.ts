8477abc3fff162fbe04847e3b1c04d2d66cd1f74
LiveSixx <tema556639@mail.ru>
2022-07-20 14:25:45 +0300

    deleted useless code

--- src/app/services/title.service.ts
+++ src/app/services/title.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-const URL = '../assets/data.json';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +8,4 @@ export class TitleService {
 
   constructor(private http: HttpClient) {}
 
- getData() {
-  console.log(URL)
-   return this.http.get(URL)
- }
 }
